49ecca2dc61132f814ddb40935a696186e5e3f9f
krishivnarainagarwal <krishivagarwal01@GMAIL.COM>
2026-08-13 23:57:36 -0400

    implement header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Button} from "@/components/ui/button";
+
+const Page = () => {
+    return (
+        <div className="flex min-h-screen home-wrapper">
+            Home
+        </div>
+    )
+}
+export default Page
